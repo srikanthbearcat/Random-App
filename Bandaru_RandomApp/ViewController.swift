f75833d1e19c8037a78de2645cb041679d3bc918
Student <s525796@nw35298.nwmissouri.edu>
2016-10-31 15:00:19 -0500

    Initial Commit

---
+++ Bandaru_RandomApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bandaru_RandomApp
+//
+//  Created by Bandaru,Sreekanth on 10/31/16.
+//  Copyright © 2016 Bandaru Sreekanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
